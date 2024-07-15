a3536ba8b19ee75d583c97bb36cff934427b0c64
Wolf <andresistuk@gmail.com>
2024-07-15 22:43:11 +0300

    "Update status code in addContactController response from 200 to 201"

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -47,7 +47,7 @@ export const getContactByIDController = async (req, res, next) => {
 export const addContactController = async (req, res) => {
   const contact = await addContact(req.body);
 
-  res.status(200).json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
     data: contact,
